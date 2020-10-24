ab77253d8f14732fb99ebd086b54357d344ba50c
Lauren Bell <laurengilbert0@gmail.com>
2020-10-24 20:28:12 +0100

    gallery image addresses changed to fb photos

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,21 +10,21 @@ class Gallery extends React.Component {
           <p className="title"></p>
           <p className="subtitle"></p>
           <figure className="image is-4by5">
-            <img src="/src/images/img_9356.jpg" alt="Australia Hawaii and Estonia signpost"/>
+            <img src="https://scontent.fltn1-1.fna.fbcdn.net/v/t1.0-9/121161208_108918370984982_2866614347516770238_n.jpg?_nc_cat=100&ccb=2&_nc_sid=e3f864&_nc_ohc=ESbLIdlyf-UAX_kDHs6&_nc_ht=scontent.fltn1-1.fna&oh=ff79f26535474b8c8c070380bbcb950c&oe=5FBA39FC" alt="Australia Hawaii and Estonia signpost"/>
           </figure>
         </article>
         <article className="tile is-child notification is-white">
           <p className="title"></p>
           <p className="subtitle"></p>
           <figure className="image is-4by5">
-            <img src="/src/images/ginandpickles.jpg" alt="gin and pickles bar signpost"/>
+            <img src="https://scontent.fltn1-2.fna.fbcdn.net/v/t1.0-9/122001487_120326439844175_7349277399281664806_n.jpg?_nc_cat=101&ccb=2&_nc_sid=8bfeb9&_nc_ohc=6Ik2OK3Y5JIAX_ruoQ2&_nc_ht=scontent.fltn1-2.fna&oh=5be264fb0d6abb36226f8f2e0ea0a92f&oe=5FB82B1B" alt="gin and pickles bar signpost"/>
           </figure>
         </article>
         <article className="tile is-child notification is-white">
           <p className="title"></p>
           <p className="subtitle"></p>
           <figure className="image is-4by5">
-            <img src="/src/images/img_9349.jpg" alt="Borneo Glastonbury signpost"/>
+            <img src="https://scontent.fltn1-1.fna.fbcdn.net/v/t1.0-9/120894320_108918530984966_4460121614470436507_n.jpg?_nc_cat=108&ccb=2&_nc_sid=e3f864&_nc_ohc=Up9fXb7S7iIAX-hCjXS&_nc_ht=scontent.fltn1-1.fna&oh=66594300a36e15458513097f9f113d3c&oe=5FBB0804" alt="Borneo Glastonbury signpost"/>
           </figure>
         </article>
       </div>
