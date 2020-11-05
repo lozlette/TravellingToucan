995293f7cacd793cb7dad45b155f4e0b5d4b35f3
Lauren Bell <laurengilbert0@gmail.com>
2020-11-05 10:58:55 +0000

    js added to make dropdown navbar burger work

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,21 @@ import React from 'react'
 import 'bulma'
 
 class Nav extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      navbarOpen: false
+    }
+
+    this.toggleNavbar = this.toggleNavbar.bind(this)
+  }
+
+  toggleNavbar() {
+    this.setState({ navbarOpen: !this.state.navbarOpen })
+  }
+
   render(){
     return (
       <nav className="navbar is-transparent is-fixed-top" role="navigation" aria-label="main navigation">
@@ -10,7 +25,8 @@ class Nav extends React.Component {
             <img src="https://scontent.fltn1-2.fna.fbcdn.net/v/t1.0-9/121009987_108918067651679_8401112923733463285_n.jpg?_nc_cat=107&ccb=2&_nc_sid=09cbfe&_nc_ohc=atOEm_R8_RYAX-ytG8e&_nc_ht=scontent.fltn1-2.fna&oh=15165f7e3fd83cf52e402c8bdfc24464&oe=5FB93F66" width="30" height="40" />
           </a>
 
-          <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <a className={`navbar-burger ${this.state.navbarOpen ? 'is-active' :'' }`}
+            onClick={this.toggleNavbar}>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
