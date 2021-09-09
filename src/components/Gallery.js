31ab3672196c861c072f746f3a66992ef28ef5ee
Lauren Bell <laurengilbert0@gmail.com>
2021-09-09 11:30:07 +0100

    change to filepath of to gallery images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,7 +10,7 @@ class Gallery extends React.Component {
           <p className="title"></p>
           <p className="subtitle"></p>
           <figure className="image is-4by5">
-            <img src="https://scontent.fltn1-1.fna.fbcdn.net/v/t1.0-9/121161208_108918370984982_2866614347516770238_n.jpg?_nc_cat=100&ccb=2&_nc_sid=e3f864&_nc_ohc=ESbLIdlyf-UAX_kDHs6&_nc_ht=scontent.fltn1-1.fna&oh=ff79f26535474b8c8c070380bbcb950c&oe=5FBA39FC" alt="Australia Hawaii and Estonia signpost"/>
+            <img src="./images/img_9356.jpg" alt="Australia Hawaii and Estonia signpost"/>
           </figure>
         </article>
         <article className="tile is-child notification is-white">
@@ -24,7 +24,7 @@ class Gallery extends React.Component {
           <p className="title"></p>
           <p className="subtitle"></p>
           <figure className="image is-4by5">
-            <img src="https://scontent.fltn1-1.fna.fbcdn.net/v/t1.0-9/120894320_108918530984966_4460121614470436507_n.jpg?_nc_cat=108&ccb=2&_nc_sid=e3f864&_nc_ohc=Up9fXb7S7iIAX-hCjXS&_nc_ht=scontent.fltn1-1.fna&oh=66594300a36e15458513097f9f113d3c&oe=5FBB0804" alt="Borneo Glastonbury signpost"/>
+            <img src="./images/img_9349.jpg" alt="Borneo Glastonbury signpost"/>
           </figure>
         </article>
       </div>
