5ddee63762b5b34243b4f815c8f2c689daab7595
Lauren Bell <laurengilbert0@gmail.com>
2022-10-24 12:57:40 +0100

    changes to gallery layout

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,22 +8,22 @@ import img9339 from '../../images/img9339.jpeg'
 class Gallery extends React.Component {
   render(){
     return (
-      <div className="tile is-parent is-dark" id="gallery">
-        <article className="tile is-child notification is-dark">
+      <div className="tile is-parent" id="gallery">
+        <article className="tile is-child notification is-white">
           <p className="title"></p>
           <p className="subtitle"></p>
           <figure className="image is-4by5">
             <img src={img9356} alt="Australia Hawaii and Estonia signpost"/>
           </figure>
         </article>
-        <article className="tile is-child notification is-dark">
+        <article className="tile is-child notification is-white">
           <p className="title"></p>
           <p className="subtitle"></p>
           <figure className="image is-4by5">
             <img src={img9339} alt="gin and pickles bar signpost"/>
           </figure>
         </article>
-        <article className="tile is-child notification is-dark">
+        <article className="tile is-child notification is-white">
           <p className="title"></p>
           <p className="subtitle"></p>
           <figure className="image is-4by5">
